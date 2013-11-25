7c36fb2eb3519c63c3f8194e59ab317ba7cf4d66
tdoherty <timmeedee@gmail.com>
2013-11-25 12:03:54 -0800

    converted tab indent to spaces

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,29 @@
 require.config({
-	paths: {
-		backbone: '../bower_components/backbone/backbone-min',
-		underscore: '../bower_components/underscore/underscore-min',
-		jquery: '../bower_components/jquery/jquery.min',
-		marionette: '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
-		'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
-		'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
-		mustache: '../bower_components/mustache/mustache',
-		text: '../bower_components/requirejs-text/text',
-		stache: '../bower_components/requirejs-mustache/stache',
-		stickit: '../bower_components/backbone.stickit/backbone.stickit',
-		templates: '../templates'
-	},
-	shim: {
-		backbone: {
-			deps: ['jquery', 'underscore'],
-			exports: 'Backbone'
-		},
-		underscore: {
-			exports: '_'
-		},
-		stickit: {
-			deps: ['backbone']
-		}
-	}
+  paths: {
+    backbone: '../bower_components/backbone/backbone-min',
+    underscore: '../bower_components/underscore/underscore-min',
+    jquery: '../bower_components/jquery/jquery.min',
+    marionette: '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
+    'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    mustache: '../bower_components/mustache/mustache',
+    text: '../bower_components/requirejs-text/text',
+    stache: '../bower_components/requirejs-mustache/stache',
+    stickit: '../bower_components/backbone.stickit/backbone.stickit',
+    templates: '../templates'
+  },
+  shim: {
+    backbone: {
+      deps: ['jquery', 'underscore'],
+      exports: 'Backbone'
+    },
+    underscore: {
+      exports: '_'
+    },
+    stickit: {
+      deps: ['backbone']
+    }
+  }
 });
 
 require(['bootstrap'], function (bootstrap) {
